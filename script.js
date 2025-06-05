31e62e29f6405b6d6ab51eb5f6eed1cc64b240bd
Cyb3rTyr <MarquesSaRodrigo@gmail.com>
2025-06-05 09:39:03 +0200

    V0.1.3

--- script.js
+++ script.js
@@ -6,14 +6,10 @@ function showPage(pageId) {
 }
 
 function copyToClipboard(text) {
-    navigator.clipboard.writeText(text).then(() => {
-        alert("Copied to clipboard!");
-    });
+    navigator.clipboard.writeText(text);
 }
 
 function copyCommand(elementId) {
     const text = document.getElementById(elementId).innerText;
-    navigator.clipboard.writeText(text).then(() => {
-        alert("Command copied!");
-    });
+    navigator.clipboard.writeText(text);
 }
